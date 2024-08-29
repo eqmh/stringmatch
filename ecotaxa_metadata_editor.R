b6eb73bd2b1a6e3f70d2725b763ab83fde183e62
Enrique Montes <enrique.montes@margarita.aoml.noaa.gov>
2024-08-29 14:38:41 -0400

    code for changing 'object_time'

--- ecotaxa_metadata_editor.R
+++ ecotaxa_metadata_editor.R
@@ -5,14 +5,17 @@
 library(readr)
 
 # read table
-sample <- "ecotaxa_ws23203_2023-09-26T02_15_45.164133_ws23203_LK_64um"
+sample <- "ecotaxa_h24013_2023-10-02T13_09_56.023512_h24013_LK_64um"
 
 path <- paste0("/Users/enrique.montes/Desktop/planktoscope/", sample, "/ecotaxa_export.tsv")
 df <- readr::read_tsv(path)
 
 # replace values under a specific column
 # This case assigns all values under 'sample_net_gear_opening' to 6000  
-df$sample_gear_net_opening[2:nrow(df)] <- 6000
+# df$sample_gear_net_opening[2:nrow(df)] <- 6000
+
+# This changes 'object_time'
+df$object_time[2:nrow(df)] <- "093500"
 
 # write TSV table
-write.table(df, file = "ecotaxa_export.tsv", sep = "\t", row.names = FALSE)
+write.table(df, file = path, sep = "\t", row.names = FALSE)
